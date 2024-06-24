3c060552ac0726f586efaae8aa4522426c0c2f65
syou149149 <syou149149@gmail.com>
2024-06-24 13:56:59 +0900

    init

---
+++ src/main/java/com/threeDucks/basicBlog/domain/Articles.java
@@ -0,0 +1,3 @@
+package com.threeDucks.basicBlog.domain;
+public class Articles {
+}
---
+++ src/test/java/com/threeDucks/basicBlog/BasicBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threeDucks.basicBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
